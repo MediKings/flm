df62d5b48050370caeee0c9c69dcc8fb991d97a7
Medi King's <jehovahmbote3@gmail.com>
2023-02-01 16:57:11 +0100

    Home page responsive

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,27 @@
 $(document).ready(()=>{
 
-    
+    $('.ceo-images').slick({
+        autoplay: true,
+        centerMode: true,
+        speed: 150,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+           {
+              breakpoint: 993,
+              settings: {
+                 slidesToShow: 2,
+                 slidesToScroll: 1,
+              }
+           },
+           {
+              breakpoint: 768,
+              settings: {
+                 slidesToShow: 1,
+                 slidesToScroll: 1,
+              }
+           },
+        ]
+     })
 
 })
